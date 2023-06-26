71df51b9c93c4799ba2b09dcf71f39d8999c58f8
Anton_Fursov <fursila02@mail.ru>
2023-06-26 20:34:02 +0300

    homework4

---
+++ task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+for (int i = 0; i < b; i++)
+{
+    result *= a;
+}
+
+Console.WriteLine(result);
---
+++ task29/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int items = 8;
+int[] array = new int[items];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
